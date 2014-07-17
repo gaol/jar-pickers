3ba53512c9a214a7322aa583ceb94d6bb254f1f1
Lin Gao <lgao@redhat.com>
2014-07-17 15:22:22 +0800

    Added to parse artifact classfication from file name

--- tools/picker.py
+++ tools/picker.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+#
+#  This scirpt is used to analyze jar information from a zip file or a directory which contains jar files
+#
+#  The script will try to find 'pom.properties' from each jar file, then get the groupId,artifactId and version. The default type of the jar is: 'jar', if the jar file name matchees: '{artifactId}-{type}-{version}jar', then the type will be parsed as: {type}.
+#
+#  It is tested against EAP 6
+#
 
 import sys
 import os
@@ -21,7 +28,7 @@ SUSPECT_JARS = "suspect_jars"
 JSON_DATA = "jsonData"
 
 def debug(message):
-  if DEBUG == True:
+  if DEBUG:
     print message
 #end of debug
 
@@ -33,10 +40,10 @@ class DEFAULT():
   """
   Default configurations
   """
-  DOWNLOAD_TMP_DIR = "%s/tmp" % os.getcwd()
-  DATA_DIR = "%s/data/products" % os.getcwd()
+  BASE_DIR = os.path.dirname(sys.argv[0])
+  DOWNLOAD_TMP_DIR = "%s/tmp" % BASE_DIR
+  DATA_DIR = "%s/data/products" % BASE_DIR
   GROUPID_FILE = "%s/groupids.ini" % DATA_DIR
-  DEBUG = False
   
 #end of class DEFAULT
 
@@ -52,8 +59,10 @@ def downloadZip(url, output):
     urlgrabber.urlgrab(url, filename=output, progress_obj=urlgrabber.progress.TextMeter())
     info('Download completed.' )
   except:
-    info("Exception on dowloading the zip file: %s" % url)
-    print sys.exc_info()[0]
+    info("Exception on dowloading the zip file from: '%s', Please check whether the url is valid." % url)
+    if DEBUG:
+      print sys.exc_info()[0]
+    sys.exit()
 # end of downloadZip
 
 
@@ -69,7 +78,9 @@ def unzipFile(zipFile, dir):
     info('Unzipping zip file completed')
   except:
     info("Exception on extracting the zip file: %s" % zipFile)
-    print sys.exc_info()[0]
+    if DEBUG:
+      print sys.exc_info()[0]
+    sys.exit()
 # end of unzip
 
 def getJarList(dir):
@@ -84,6 +95,49 @@ def getJarList(dir):
   return jars
 # end of getJarList
 
+def parseFromPom(zf, pomFile):
+  groupId = None
+  artifactId = None
+  version = None
+  zxf=zf.open(pomFile)
+  for line in zxf.readlines():
+    if "groupId=" in line:
+      groupId = line[line.index("=") + 1:].rstrip('\n').rstrip('\r')
+    if "artifactId=" in line:
+      artifactId= line[line.index("=") + 1:].rstrip('\n').rstrip('\r')
+    if "version=" in line:
+      version = line[line.index("=")+ 1:].rstrip('\n').rstrip('\r')
+  zxf.close()
+  return groupId, artifactId, version
+  
+# end of parseFromPom
+
+def parserFromFileName(jar):
+  artifactId = None
+  version = None
+  jarFileName = os.path.basename(jar)[:-4]
+  debug("Jar file basename is: %s" % jarFileName)
+  nameSearch = VERSION_RE.search(jarFileName)
+  if not nameSearch is None:
+    versionStart = nameSearch.start()
+    artifactId = jarFileName[:versionStart]
+    version = jarFileName[versionStart + 1:]
+  return None, artifactId, version
+  
+# end of parserFromFileName
+
+def guessJarType(jar, artifactId, version):
+  """
+   Jar name is like: {artifactId}-{version}-{type}.jar
+  """
+  jarFileName = os.path.basename(jar)[:-4]
+  composedFileName = "%s-%s" % (artifactId, version)
+  if jarFileName == composedFileName:
+    return "jar"
+  if jarFileName.startswith(composedFileName):
+    return jarFileName[len(composedFileName) + 1:]
+  return "jar"
+# end of guessJarType
 
 def getArtifactInfo(jar):
   """
@@ -93,40 +147,30 @@ def getArtifactInfo(jar):
   groupId = None
   artifactId = None
   version = None
+  artiType = "jar"
   pomExist = False
   debug("Try to read pom.properties from jar: %s" % jar)
   try:
     zf=zipfile.ZipFile(jar)
     for name in zf.namelist():
-      # find pom.properties
+      # parse jar information from pom.properties
       if "pom.properties" in name:
-        debug("Find pom.properties in jar: %s" % jar)
-        zxf=zf.open(name)
         pomExist = True
-        for line in zxf.readlines():
-          if "groupId=" in line:
-            groupId = line[line.index("=") + 1:].rstrip('\n').rstrip('\r')
-          if "artifactId=" in line:
-            artifactId= line[line.index("=") + 1:].rstrip('\n').rstrip('\r')
-          if "version=" in line:
-            version = line[line.index("=")+ 1:].rstrip('\n').rstrip('\r')
-        zxf.close()
+        debug("Find pom.properties in jar: %s" % jar)
+        groupId, artifactId, version = parseFromPom(zf, name)
+        break
     zf.close()
   except:
     info("Error when reading pom.properties from jar: %s " % jar)
-    print sys.exc_info()[0]
+    if DEBUG:
+      print sys.exc_info()[0]
     pomExist = False
 
   if pomExist is False:
-    jarFileName = os.path.basename(jar)[:-4]
-    debug("Jar file basename is: %s" % jarFileName)
-    debug("No pom.properties found in jar: %s" % jar)
-    nameSearch = VERSION_RE.search(jarFileName)
-    if not nameSearch is None:
-      versionStart = nameSearch.start()
-      artifactId = jarFileName[:versionStart]
-      version = jarFileName[versionStart + 1:]
-  return groupId, artifactId, version
+    # try to parse the jar information from file name
+    groupId, artifactId, version = parserFromFileName(jar)
+  artiType = guessJarType(jar, artifactId, version)
+  return groupId, artifactId, version, artiType
 #end of getArtifactInfo
 
 class Picker():
@@ -176,17 +220,15 @@ class Picker():
   def picks(self, request):
     """
     Starts to pick jars information up.
-    request is the dict data which contains name, version, milestone, urls
+    request is the dict data which contains name, version, urls
     name : the name of the product
     version : the version of the product
-    milestone : the milestone of the product, this is optional
     urls : the urls where to download the product zip files
     """
     if request is None:
       raise Exception("No request information")
     name = request.get("name", None)
     version = request.get("version", None)
-    milestone = request.get("milestone", None)
     urls = request.get("urls", [])
 
     if name is None or version is None or (urls is None or len(urls) == 0):
@@ -206,11 +248,11 @@ class Picker():
       fileName = "%s/%s" % (self.tmpDir, os.path.basename(url))
       dirName = "%s/tmp-%s" % (self.tmpDir, os.path.basename(url))
       if os.path.isfile(url):
-        info("%s is a local zip file" % url)
+        info("'%s' is a local zip file" % url)
         isLocalFile = True
         fileName = url
       if os.path.isdir(url):
-        info("%s is a local directory" % url)
+        info("'%s' is a local directory" % url)
         isLocalDir = True
         dirName = url
       if isLocalFile or isLocalDir:
@@ -219,8 +261,9 @@ class Picker():
         downloadZip(url, fileName)
       if isLocalDir is False:
         unzipFile(fileName, dirName)
+      info("Starts parsing jar information at directory: '%s'" % dirName)
       for jar in getJarList(dirName):
-        groupId, artifactId, artiVersion = getArtifactInfo(jar)
+        groupId, artifactId, artiVersion, artiType = getArtifactInfo(jar)
         if artifactId is None or artiVersion is None:
           info("WARNING: Can't parse jar: %s" % jar)
           errors.append("Error: %s" % os.path.basename(jar))
@@ -230,7 +273,7 @@ class Picker():
         if groupId is None:
           info("No groupId found in jar file: %s" % os.path.basename(jar))
           errors.append("Warning: No GroupId: %s" % os.path.basename(jar))
-        artifactstr = "%s:%s:%s:jar" % (groupId, artifactId, artiVersion)
+        artifactstr = "%s:%s:%s:%s" % (groupId, artifactId, artiVersion, artiType)
         if not artifactstr in artifacts:
           artifacts.append(artifactstr)
       if isLocalDir:
@@ -242,9 +285,7 @@ class Picker():
     if not os.path.exists(self.dataDir):
       info("Data directory does not exist, create it.")
       os.makedirs(self.dataDir)
-    outputFile = "%s/%s-%s-%s.list" % (self.dataDir, name, version, milestone)
-    if milestone is None:
-      outputFile = "%s/%s-%s.list" % (self.dataDir, name, version)
+    outputFile = "%s/%s-%s.list" % (self.dataDir, name, version)
     output = file(outputFile, 'w')
     for artiLine in artifacts:
       output.write(artiLine + "\n")
@@ -265,23 +306,29 @@ def main():
   """
    Main entrance for command line.
   """
-  parser = optparse.OptionParser(usage='%prog [options]')
-  parser.add_option('--debug', dest='debug', help='Print debug message', action='store_true', default = True)
-  parser.add_option('-n', '--name', dest='name', help='Specify JBoss product short name. Like: eap, ews, etc.')
-  parser.add_option('-v', '--version', dest='version', help='Specify JBoss product version')
-  parser.add_option('-m', '--milestone', dest='milestone', help='Specify JBoss product milestone')
-  parser.add_option('-u', '--urls', dest='urls', type='string', help='Specify zip files url.', action='callback', callback=listValues)
+  usage="%prog [options]"
+  description="""  This script is tested against EAP 6.
+  		   For EAP 5, please use eap5.py
+  		   name, version and urls must be provided."
+  """
+  parser = optparse.OptionParser(usage=usage, description = description)
+  parser.add_option('-d', '--debug', dest='debug', action='store_true', help='Print debug message', default = False)
+  parser.add_option('-n', '--name', dest='name', help='Name of the product, like: eap, ews, etc. This is required')
+  parser.add_option('-v', '--version', dest='version', help='Version of the product, like: 6.2.4,6.2.3.ER3, etc. This is required')
+  parser.add_option('-u', '--urls', dest='urls', type='string', help='URLs of the product zip file, or local directory. This is required', action='callback', callback=listValues)
   options, args = parser.parse_args()
-  debug = options.debug
+  global DEBUG
+  DEBUG = options.debug
   name = options.name
   version = options.version
-  milestone = options.milestone
   urls= options.urls
-  if name is None:
+  if name is None or version is None or urls is None:
     parser.print_help()
     sys.exit(1)
   picker = Picker()
-  print picker.picks({"name" : name, "version" : version, "milestone" : milestone, "urls" : urls})
+  result = picker.picks({"name" : name, "version" : version, "urls" : urls})
+  if len(result) != 0:
+    print result
 #end of main
 
 
