d4d07fa83e687ba40579570aeaf8a96df2276f76
Lin Gao <lgao@redhat.com>
2014-08-04 16:45:46 +0800

    Prefer IPV4

--- irc-bot/ircbot.sh
+++ irc-bot/ircbot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cli_jar=`find . -name "irc-bot-*-cli.jar" -a -type f`
 if [ "${cli_jar}" != "" ]; then
-  exec "`which java`" -jar ${cli_jar} "$@"
+  exec "`which java`" -Djava.net.preferIPv4Stack=true  -jar ${cli_jar} "$@"
 fi
